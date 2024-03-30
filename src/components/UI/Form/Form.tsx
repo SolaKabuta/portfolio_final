3872b9fc970a71a357817d606c33f8bad131d1ad
Sola <solakabuta@gmail.com>
2024-03-30 22:27:49 +0100

    feat: adding form

---
+++ src/components/UI/Form/Form.tsx
@@ -0,0 +1,36 @@
+import './Form.css'
+const Form = () => {
+    return (
+        <>
+            <div>
+                <div
+                    className={'flex justify-center px-20'}>
+                    <form action="">
+                        <div
+                            className={'names'}>
+                            <input type="text" placeholder={'First name'}/>
+                            <input type="text" placeholder={'Second name'}/>
+                        </div>
+                        <br/>
+                        <div>
+                            <input type="text" placeholder={'janedoe@gmail.com'}/>
+                        </div>
+                        <br/>
+                        <div>
+                            <input className={'h-52'} type="text" placeholder={'Write something'}/>
+                        </div>
+                        <div
+                            className={'flex justify-center'}>
+                            <button
+                                className={'send-btn '}
+                                type={'submit'}>Send
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Form;
--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -1,4 +1,6 @@
 import Footer from "../../components/UI/Footer/Footer.tsx";
+import Form from "../../components/UI/Form/Form.tsx";
+import './Contact.css'
 
 const Contact = () => {
     return (
@@ -14,44 +16,12 @@ const Contact = () => {
                         <span className={'text-orange-600'}>&nbsp;together</span>.
                     </h4>
                     <p
-                        className={'grid place-content-center pb-10 text-center ease-out duration-1000 hover:scale-[1.05]'}>
+                        className={'contact-text'}>
                         <span>If you are curious about my process or are interested in the details, <br/></span>
                         <span>Feel free to reach out to me.</span>
                     </p>
                     {/*FORM*/}
-                    <div
-                        className={'flex justify-center px-20'}>
-                            <form action="">
-                                <div
-                                    className={'names'}>
-                                    <input type="text" placeholder={'First name'}/>
-                                    <input type="text" placeholder={'Second name'}/>
-                                </div>
-                                <br/>
-                                <div>
-                                    <input type="text" placeholder={'janedoe@gmail.com'}/>
-                                </div>
-                                <br/>
-                                <div>
-                                    <input className={' h-52'} type="text" placeholder={'Write something'}/>
-                                </div>
-                                <div
-                                    className={'flex justify-center'}>
-                                    <button
-                                        className={'send-btn '}
-                                        type={'submit'}>Send
-                                    </button>
-                                </div>
-                            </form>
-                    </div>
-                    {/*<a*/}
-                    {/*    href="mailto:solakabuta@gmail.com">*/}
-                    {/*    solakabuta@gmail.com*/}
-                    {/*</a>*/}
-                    {/*<a*/}
-                    {/*    href='tel:+32 474 73 08 10'>*/}
-                    {/*    +32 474 73 08 10*/}
-                    {/*</a>*/}
+                    <Form/>
                 </div>
             </div>
                 <Footer/>
