770aeb8c7a1b3a075354432817e1b4842ae67ed5
Sola <solakabuta@gmail.com>
2024-03-31 17:19:00 +0200

    style: links target

--- src/components/UI/Footer/Footer.tsx
+++ src/components/UI/Footer/Footer.tsx
@@ -6,9 +6,9 @@ const Footer = () => {
                 className={'w-screen py-1 px-[10%] footer-nav'}>
                     <div
                         className={'flex justify-between align-middle'}>
-                        <a href="https://github.com/SolaKabuta">Github</a>
-                        <a href="https://www.linkedin.com/in/sola-kabuta/">Linkedin</a>
-                        <a href="https://dribbble.com/SolaKabuta">Dribbble</a>
+                        <a target="_blank" href="https://github.com/SolaKabuta">Github</a>
+                        <a target="_blank" href="https://www.linkedin.com/in/sola-kabuta/">Linkedin</a>
+                        <a target="_blank" href="https://dribbble.com/SolaKabuta">Dribbble</a>
                     </div>
             </div>
         </>
