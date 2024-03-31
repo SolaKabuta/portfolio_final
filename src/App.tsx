4f62238c1fed645f2daae093fd79bb6d6f3460e7
Sola <solakabuta@gmail.com>
2024-03-31 19:05:35 +0200

    feat: lazy loading components

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,34 @@
-// import { useState } from 'react'
-// import { BrowserRouter, Route, Routes } from "react-router-dom";
-
-// Mouse trail
+import React, { Suspense } from 'react';
 import { MouseTrail } from "@stichiboi/react-elegant-mouse-trail";
+import './App.css';
+import Navbar from './components/UI/Navbar/Navbar';
+import logo from './assets/icons/logo/logo_white.svg'
 
-
-import './App.css'
-import HomePage from "./pages/Home/HomePage.tsx";
-import Navbar from "./components/UI/Navbar/Navbar.tsx";
-import Work from "./pages/Work/Work.tsx";
-import About from "./pages/About/About.tsx";
-import Contact from "./pages/Contact/Contact.tsx";
-import HorizontalScrollCarousel from "./components/Features/WorkCarousel/HorizontalScrollCarousel.tsx";
-// import Footer from "./components/Footer.tsx";
-// import Home_btn from "./components/home_btn.tsx";
+// Lazy load components
+const LazyHomePage = React.lazy(() => import('./pages/Home/HomePage'));
+const LazyWork = React.lazy(() => import('./pages/Work/Work'));
+const LazyAbout = React.lazy(() => import('./pages/About/About'));
+const LazyContact = React.lazy(() => import('./pages/Contact/Contact'));
+const LazyHorizontalScrollCarousel = React.lazy(() => import('./components/Features/WorkCarousel/HorizontalScrollCarousel'));
 
 function App() {
-  // const [count, setCount] = useState(0)
-
-  return (
-    <>
-        {/*<div*/}
-        {/*        className={'sticky'}>*/}
-        {/*        <Home_btn/>*/}
-        {/*</div>*/}
-        <div
-            className={'nocursor'}>
-            <MouseTrail className={'mix-blend-difference'} strokeColor={"#ff5722"} lineWidthStart={20} lag={0.7} lineDuration={2}/>
-            <Navbar/>
-            <HomePage/>
-            <Work/>
-            <HorizontalScrollCarousel/>
-            <About/>
-            <Contact/>
-        </div>
-    </>
-  )
+    return (
+        <>
+            <div className="nocursor">
+                {/* Suspense component to display a fallback while components are loading */}
+                <Suspense fallback={<div className={'w-screen h-screen grid place-content-center  animate-pulse bg-black'}>
+                    <img src={logo} alt=""/></div>}>
+                    <MouseTrail className={'mix-blend-difference'} strokeColor={"#ff5722"} lineWidthStart={20} lag={0.7} lineDuration={2}/>
+                    <Navbar />
+                    <LazyHomePage />
+                    <LazyWork />
+                    <LazyHorizontalScrollCarousel />
+                    <LazyAbout />
+                    <LazyContact />
+                </Suspense>
+            </div>
+        </>
+    );
 }
 
-export default App
+export default App;
